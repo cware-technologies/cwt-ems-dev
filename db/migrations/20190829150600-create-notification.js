da77818b140d80ba710e7f6f80a2e4550593135c
Sajeel Waien <sajeel_waien@live.com>
2019-08-29 20:10:45 +0500

    notification migration

---
+++ db/migrations/20190829150600-create-notification.js
@@ -0,0 +1,138 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('C_NOTIFY', {
+        row_id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER(11)
+        },
+        emp_id: {
+          type: Sequelize.INTEGER(11),
+          references: {
+            model: 'C_EMP',
+            key: 'row_id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        created: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        stat_cd: {
+          type: Sequelize.STRING(30),
+          allowNull: false,
+          defaultValue: 'pending',
+        },
+        bu_id: {
+          type: Sequelize.INTEGER(11),
+          references: {
+            model: 'C_BU',
+            key: 'row_id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        type_cd: {
+          type: Sequelize.STRING(200),
+        },
+        ATTRIB_01: {
+          type: Sequelize.STRING(200),
+        },
+        ATTRIB_02: {
+          type: Sequelize.STRING(200),
+        },
+        ATTRIB_03: {
+          type: Sequelize.STRING(200),
+        },
+        ATTRIB_04: {
+          type: Sequelize.STRING(200),
+        },
+        ATTRIB_05: {
+          type: Sequelize.STRING(100),
+        },
+        ATTRIB_06: {
+          type: Sequelize.STRING(100),
+        },
+        ATTRIB_07: {
+          type: Sequelize.STRING(100),
+        },
+        ATTRIB_08: {
+          type: Sequelize.STRING(100),
+        },
+        ATTRIB_09: {
+          type: Sequelize.STRING(100),
+        },
+        ATTRIB_10: {
+          type: Sequelize.STRING(100),
+        },
+        ATTRIB_11: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_12: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_13: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_14: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_15: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_16: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_17: {
+          type: Sequelize.INTEGER(11),
+        },
+        ATTRIB_18: {
+          type: Sequelize.DATE,
+        },
+        ATTRIB_19: {
+          type: Sequelize.DATE,
+        },
+        ATTRIB_20: {
+          type: Sequelize.DATE,
+        },
+        FLG_01: {
+          type: Sequelize.BOOLEAN,
+        },
+        FLG_02: {
+          type: Sequelize.BOOLEAN,
+        },
+        FLG_03: {
+          type: Sequelize.BOOLEAN,
+        },
+        FLG_04: {
+          type: Sequelize.BOOLEAN,
+        },
+        FLG_05: {
+          type: Sequelize.BOOLEAN,
+        },
+        FLG_06: {
+          type: Sequelize.CHAR,
+        },
+        FLG_07: {
+          type: Sequelize.CHAR,
+        },
+        FLG_08: {
+          type: Sequelize.CHAR,
+        },
+        FLG_09: {
+          type: Sequelize.CHAR,
+        },
+        FLG_10: {
+          type: Sequelize.CHAR,
+        },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('C_NOTIFY');
+  }
+};
--- db/models/notifications.js
+++ db/models/notifications.js
@@ -9,7 +9,12 @@ module.exports = (sequelize, DataTypes) => {
     },
     emp_id: {
       type: DataTypes.INTEGER(11),
-      
+      references: {
+        model: 'C_EMP',
+        key: 'row_id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
     },
     created: {
       type: DataTypes.DATE,
@@ -22,13 +27,16 @@ module.exports = (sequelize, DataTypes) => {
     },
     bu_id: {
       type: DataTypes.INTEGER(11),
-
+      references: {
+        model: 'C_BU',
+        key: 'row_id',
+      },
       onUpdate: 'CASCADE',
       onDelete: 'CASCADE',
     },
-    // type_cd: {
-    //   type: DataTypes.STRING(200),
-    // },
+    type_cd: {
+      type: DataTypes.STRING(200),
+    },
     ATTRIB_01: {
       type: DataTypes.STRING(200),
     },
@@ -38,93 +46,87 @@ module.exports = (sequelize, DataTypes) => {
     ATTRIB_03: {
       type: DataTypes.STRING(200),
     },
-    // ATTRIB_04: {
-    //    type: DataTypes.STRING(200),
-    //  },
-    // ATTRIB_05: {
-    //   type: DataTypes.STRING(100),
-    // },
-    // ATTRIB_06: {
-    //   type: DataTypes.STRING(100),
-    // },
-    // ATTRIB_07: {
-    //   type: DataTypes.STRING(100),
-    // },
-    // ATTRIB_08: {
-    //   type: DataTypes.STRING(100),
-    // },
-    // ATTRIB_09: {
-    //   type: DataTypes.STRING(100),
-    // },
-    // ATTRIB_10: {
-    //   type: DataTypes.STRING(100),
-    // },
-    // ATTRIB_11: {
-    //   type: DataTypes.INTEGER(11),
-    //     references: {
-    //       'model': 'C_LST_VAL',
-    //       'key': 'row_id',
-    //     },
-    //     onUpdate: 'CASCADE',
-    //     onDelete: 'CASCADE',
-    // },
-    // ATTRIB_12: {
-    //   type: DataTypes.INTEGER(11),
-    // },
-    // ATTRIB_13: {
-    //   type: DataTypes.INTEGER(11),
-    // },
-    // ATTRIB_14: {
-    //   type: DataTypes.INTEGER(11),
-    // },
-    // ATTRIB_15: {
-    //   type: DataTypes.INTEGER(11),
-    // },
-    // ATTRIB_16: {
-    //   type: DataTypes.INTEGER(11),
-    // },
-    // ATTRIB_17: {
-    //   type: DataTypes.INTEGER(11),
-    // },
-    // ATTRIB_18: {
-    //   type: DataTypes.DATE,
-    // },
-    // ATTRIB_19: {
-    //   type: DataTypes.DATE,
-    // },
-    // ATTRIB_20: {
-    //   type: DataTypes.DATE,
-    // },
-    // FLG_01: {
-    //   type: DataTypes.BOOLEAN,
-    // },
-    // FLG_02: {
-    //   type: DataTypes.BOOLEAN,
-    // },
-    // FLG_03: {
-    //   type: DataTypes.BOOLEAN,
-    // },
-    // FLG_04: {
-    //   type: DataTypes.BOOLEAN,
-    // },
-    // FLG_05: {
-    //   type: DataTypes.BOOLEAN,
-    // },
-    // FLG_06: {
-    //   type: DataTypes.CHAR,
-    // },
-    // FLG_07: {
-    //   type: DataTypes.CHAR,
-    // },
-    // FLG_08: {
-    //   type: DataTypes.CHAR,
-    // },
-    // FLG_09: {
-    //   type: DataTypes.CHAR,
-    // },
-    // FLG_10: {
-    //   type: DataTypes.CHAR,
-    // },
+    ATTRIB_04: {
+       type: DataTypes.STRING(200),
+     },
+    ATTRIB_05: {
+      type: DataTypes.STRING(100),
+    },
+    ATTRIB_06: {
+      type: DataTypes.STRING(100),
+    },
+    ATTRIB_07: {
+      type: DataTypes.STRING(100),
+    },
+    ATTRIB_08: {
+      type: DataTypes.STRING(100),
+    },
+    ATTRIB_09: {
+      type: DataTypes.STRING(100),
+    },
+    ATTRIB_10: {
+      type: DataTypes.STRING(100),
+    },
+    ATTRIB_11: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_12: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_13: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_14: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_15: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_16: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_17: {
+      type: DataTypes.INTEGER(11),
+    },
+    ATTRIB_18: {
+      type: DataTypes.DATE,
+    },
+    ATTRIB_19: {
+      type: DataTypes.DATE,
+    },
+    ATTRIB_20: {
+      type: DataTypes.DATE,
+    },
+    FLG_01: {
+      type: DataTypes.BOOLEAN,
+    },
+    FLG_02: {
+      type: DataTypes.BOOLEAN,
+    },
+    FLG_03: {
+      type: DataTypes.BOOLEAN,
+    },
+    FLG_04: {
+      type: DataTypes.BOOLEAN,
+    },
+    FLG_05: {
+      type: DataTypes.BOOLEAN,
+    },
+    FLG_06: {
+      type: DataTypes.CHAR,
+    },
+    FLG_07: {
+      type: DataTypes.CHAR,
+    },
+    FLG_08: {
+      type: DataTypes.CHAR,
+    },
+    FLG_09: {
+      type: DataTypes.CHAR,
+    },
+    FLG_10: {
+      type: DataTypes.CHAR,
+    },
   },
   {
     timestamp: true,
@@ -132,10 +134,10 @@ module.exports = (sequelize, DataTypes) => {
     updatedAt: false,
     createdAt: 'created',
   });
-//   AdminRequest.associate = function(models) {
-//     AdminRequest.belongsTo( models.C_EMP, { as: 'requestor', foreignKey: 'emp_id' })
-//     AdminRequest.belongsTo( models.C_BU, { as: 'organization', foreignKey: 'bu_id' })
+  AdminRequest.associate = function(models) {
+    AdminRequest.belongsTo( models.C_EMP, { as: 'employee', foreignKey: 'emp_id' })
+    AdminRequest.belongsTo( models.C_BU, { as: 'organization', foreignKey: 'bu_id' })
 //     AdminRequest.belongsTo( models.C_LST_VAL, { as: 'expenseType', foreignKey: 'ATTRIB_11' })
-//   };
+  };
   return Notification;
 };
